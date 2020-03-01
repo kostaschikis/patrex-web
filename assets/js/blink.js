09608b6cf6de43401cdf528f641ffc6238dd88fa
Tressos-Aristomenis <ninjacawboyzz@gmail.com>
2020-03-02 00:45:19 +0200

    fixed blink bug

--- assets/js/blink.js
+++ assets/js/blink.js
@@ -1,8 +1,7 @@
 // Get the Paragraph Hash
 var hash = location.hash.substr(1);
-
 // When document is loaded remove 'is-preload' class from body
 $(document).ready(function() {
     $('body').removeClass('is-preload');
-    $('h3#' + hash).addClass('blinking');
+    $('h3#' + hash + "_h3").addClass('blinking');
 })
