d80694be812095c1bc51480900f9773d2359e365
Tressos-Aristomenis <ninjacawboyzz@gmail.com>
2020-03-02 00:12:54 +0200

    removed php files

---
+++ assets/js/service-texts.js
@@ -0,0 +1,58 @@
+	const T_WD = "We design and develop responsive websites with a particular emphasis on aesthetics. We implement original and fully functional websites to ensure that your website visitors will have the best possible user experience.";
+    const T_EC = "Our goal is the development of fully functional and increased traffic E-shops with a unique design to match your brand. PatreX digital agency provides customers with a secure connection to complete their orders with their credit card or PayPal.";
+    const T_SUPP = "We have the know-how to offer a total monthly support and maintenance services to your company’s website. We implement a set of different service tasks that aim to protect the website from several threats. After a hard-working period, we succeeded in creating an innovative security scan system which scans each website and it is able to detect any potential vulnerabilities  (functionality, reduced traffic, hackers etc.) on a monthly basis. A CSV file will be available for each one of our clients to review the monthly report.";
+    const T_WR = "More and more companies seem to have deficient websites. The website is the identity of your company. PatreX is here to renovate the entire website of your company with the most innovative graphics, security systems and with content that will represent the ID of your company with the best way possible.";
+    const T_SM = "Almost every business has at least one social media profile, making each social media network important for any business. Create your own community, interact with your customers and find new ones by expanding your company. At the point where traditional advertising subsides for the sake of interactivity, we recommend you to take full advantage of the huge potential offered by a professional Social Media management package.";
+    const T_SEO = "The promotion of websites with Search Engine Optimization (SEO) is one of the most effective methods in the field of Digital Marketing. It is considered a real investment for a company, as it results in increased traffic to your website. It also helps increase Likes on Facebook and other Social Media and ultimately increase your engagement and sales.<br />With the improvement of SEO, revenue is growing steadily, as well as Brand Awareness, which will bring future customers as well. Wondering what is the goal of SEO? It’s to drive your site to the first page of Google’s results when users search for specific keywords. The reason an SEO Company always aims at the first page is that users rarely go into the process of visiting another page.<br />The keywords are selected by searching for competition, monthly search volume and relevance to your business. In this way and once the SEO work is completed, your business has the opportunity to appear to prospective customers while they are looking for your services or products. Additionally, promoting websites with SEO does not have the CPC charges that Adwords uses. Once your page appears on the first page of organic results, there are no billings per click.<br />The algorithm used by Google to rank your site contains about 200 factors that are constantly changing. Thus, after the initial process of SEO site promotion has been completed, an SEO company needs to make the necessary modifications. In this way, your website will remain in high ranks.";
+    const T_DS = "At PatreX Digital Agency, we aim to make Social Media a professional tool that will deliver results."
+                + "The first steps to achieve these goals require your help and are the following:"
+                + "<br />"
+                + "<ul style='width: 90%;margin:0 auto;list-style-type:circle;'>"
+                    + "<li>Company Description. Description of your company and your business sector."
+                    + "We will continue to investigate your competition so that we can choose the right solutions and always be one step ahead.</li>"
+                    + "<li>Choose your social media channels."
+                    + "Depending on the type of business, we recommend the appropriate Social Media with the best performance based on our experience."
+                    + "Some types of businesses work better with Facebook, some with Instagram or Twitter, and some others may need a combination.</li>"                    
+                    + "<li>Strategic planning. We create the appropriate strategy for each Social Media you choose and we proceed to its implementation.</li>"
+                    + "<li>Create profiles. We create professional profiles on the Social Media channels of your choice, the necessary graphics, and texts."
+                    + "<li>Profile management. We continue with the daily management of your profile, publishing material (graphic, photos, videos, etc.)"
+                    + "promoting the page and some selected posts, as well as responding to user feedback.</li>"
+                    + "<li>Monitoring and Optimisation. Monitoring of the page and all advertising campaigns, performance control and optimisation.</li>"
+                + "</ul>";
+    const T_CC = "Nowadays, the competition between companies can beis really painful and sometimes disastrous, especially for the small ones. PatreX digital agency is here to optimize every digital aspect of your business. We present you a system with several steps that contribute to your growth because everything we do in our business and the way we do it, plays a very important role in our success.";
+    const T_IS = "There are a lot of companies that cannot face the modern trade system requirements. PatreX team has the creativity, experience and know-how in order to suggest and implement unique and innovative solutions.";
+    const T_LCID = "We build unique websites that follow your brand identity and increase brand awareness, helping you stand out and overcome your competition. The logo or brand name of the company is one of the most important components for a successful branding strategy. It is the visual symbol that represents your business and is imprinted in your subconscious as the heart of your corporate identity.  We can help you create or renew your unique brand identity. Corporate identity is the visual image and the means by which you transmit the style, activity and philosophy of your business which are the three components that make up the uniqueness of your business.";
+    const T_PCSM = "Modern graphics design and the use of appropriate colours and graphics in social media play an important role in order to achieve a balanced overall picture of your business on the internet and to stand out from the competition.";
+    const T_BDDB = "A thoughtful and stylish design of banners positively affects the user’s psychology and increases visits to your website. Moreover, we create extraordinary designs for your company's brochures that every competitor would be jealous of.";
+    const T_WUCC = "Over the last few years, more and more people are getting into technology."
+                + "Its impact is large on our daily lives. The Web is by far the most used feature that technology offers."
+                + "<br />"
+                + "You may be lost when you get into something new, such as the Web's world."
+                + "All of us need a helping hand so we won't get lost in the unknown. PatreX is the helping hand for you."
+                + "We are doing step-by-step lessons on the Web. We are friendly, patient and willing to teach."
+                + "We reinforce face-to-face lessons because, scientificly proven, students can organize their thoughts more efficiently than in cases of non face-to-face lessons."
+                + "Also human interaction can be really helpful into better understanding of the topic. The target audience can vary to any age."
+                + "More specifically, the lessons are related to:"
+                + "<br />"
+                + "<ul style='width: 90%;margin:0 auto;list-style-type:circle;'>"
+                    + "<li>Efficient web usage.</li>"
+                    + "<li>Shortcut tricks that will save you some time from doing boring tasks over and over again.</li>"
+                    + "<li>Advices on how to complete tasks of your daily work online</li>"
+                + "</ul>"
+                + "We are open to answer any questions of yours as well!";
+    const T_DIY = "We live in the era of technology so people's adjustment in this era is important. Computers make up a significant impact on our lives and a huge number of houses has at least one computer today. Again just like the web, people might get lost into the big world of computers, so a helping hand is necessary to guide you through. We carry out step-by-step lessons on how to use the computer for basic things and how you can digitalize some of your tasks to save time and space. We can help you adjust very easily to the era of technology and answer any questions you have about computers.<br />Nevertheless, the ultimate goal of this course is to teach you how to be independent in the world of technology. We promote the DIY (Do-It-Yourself) method. Our task is to make you be able to solve any problem you face, having Google as your only assistant.";
+    const T_SIBC = "As internet is getting more into our lives, conscious and sane use of the internet is significant."
+                + "Many dangers exist on the Internet and most importantly, usually they are hidden."
+                + "Being aware of these dangers can keep you safe from falling into traps set by malicious users."
+                + "We are here for you to offer all the important information on how to be safe on the internet."
+                + "More specifically, we are willing to inform you how to fulfil the things below safer:"
+                + "<br />"
+                + "<ul style='width: 90%;margin:0 auto;list-style-type:circle;'>"
+                    + "<li>Online Payment.</li>"
+                    + "<li>Web Browsing.</li>"
+                    + "<li>File Downloading.</li>"
+                + "</ul>"
+                + "and much more ..."
+                + "<br />"
+                + "Of course we are here for you to answer any question you have!";
+    const T_GA = "We offer a custom google analytics set up for our client’s company. After building web pages, we analyze their performance through analysis and reporting of demographics and visitor behavior while browsing. This helps you better understand your audience and improve your services.";
