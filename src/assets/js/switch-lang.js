54f609255d30e464faae83041151f319085e97ac
Kostas Chikimtzis <kostaschikis@gmail.com>
2020-03-07 16:45:46 +0200

    Lang Switch Working + Organized Dir

---
+++ src/assets/js/switch-lang.js
@@ -0,0 +1,9 @@
+// Switch to English
+const langEn = document.getElementById('lang-en').addEventListener('click', function() {
+    window.location.href = '../en';
+});
+
+// Swicth to Greek
+const langEl = document.getElementById('lang-el').addEventListener('click', function() {
+    window.location.href = '../el';
+});
